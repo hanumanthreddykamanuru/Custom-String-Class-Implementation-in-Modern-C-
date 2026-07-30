2728f6ac6ec27031305322c0de2d63046970e3a2
hanumanthreddykamanuru <kamanuruhanumanthreddy@gmail.com>
2026-07-30 13:03:33 +0000

    Constructors and Destructor

---
+++ mini.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+class string
+{
+	char *str;
+	public:
+	string()										//Constructor
+	{
+		str=new char[1];
+		str[0]='\0';
+	}
+	string(const char *t)							//Parameterized Constructor
+	{
+		str=new char[strlen(s)+1];
+		strcpy(str,t);
+	}
+	string(string &t)								//Copy Constructor
+	{
+		str=new char[strlen(t.str)+1];
+		strcpy(str,t.str);
+	}
+	~string()										//Destructor
+	{
+		delete []str;
+	}
+}
+int main()
+{
+}
+
