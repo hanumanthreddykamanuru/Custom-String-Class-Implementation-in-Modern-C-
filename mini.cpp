73733e2a6326fea6ccc499ab9f7efb4f4248e5f7
hanumanthreddykamanuru <kamanuruhanumanthreddy@gmail.com>
2026-07-30 13:34:51 +0000

    Operators ==, !=, <, >, <=, >=

--- mini.cpp
+++ mini.cpp
@@ -1,30 +1,65 @@
 #include<iostream>
+#include<cstring>
 using namespace std;
-class string
+class String
 {
 	char *str;
 	public:
-	string()										//Constructor
+	String()										// Constructor
 	{
 		str=new char[1];
 		str[0]='\0';
 	}
-	string(const char *t)							//Parameterized Constructor
+	String(const char *t)							// Parameterized Constructor
 	{
 		str=new char[strlen(t)+1];
 		strcpy(str,t);
 	}
-	string(string &t)								//Copy Constructor
+	String(String &t)								// Copy Constructor
 	{
 		str=new char[strlen(t.str)+1];
 		strcpy(str,t.str);
 	}
-	~string()										//Destructor
+	~String()										// Destructor
 	{
 		delete []str;
 	}
-}
+	bool operator==(String &t)						// Operator ==
+	{
+		return (strcmp(str,t.str)==0);
+	}
+	bool operator!=(String &t)						// Operator !=
+	{
+		return (strcmp(str,t.str)!=0);
+	}
+	bool operator<(String &t)						// Operator <
+	{
+		return (strcmp(str,t.str)<0);
+	}
+	bool operator>(String &t)						// Operator >
+	{
+		return (strcmp(str,t.str)>0);
+	}
+	bool operator<=(String &t)						// Operator <=
+	{
+		return (strcmp(str,t.str)<=0);
+	}
+	bool operator>=(String &t)						// Operator >=
+	{
+		return (strcmp(str,t.str)>=0);
+	}
+};
 int main()
 {
+	String s1("Vector"),s2(s1),s3;
+	cout << boolalpha;
+
+cout << "s1==s2 : " << (s1 == s2) << endl;
+cout << "s1!=s2 : " << (s1 != s2) << endl;
+cout << "s1<s2  : " << (s1 < s2) << endl;
+cout << "s1>s2  : " << (s1 > s2) << endl;
+cout << "s1<=s2 : " << (s1 <= s2) << endl;
+cout << "s1>=s2 : " << (s1 >= s2) << endl;
+
 }
 
